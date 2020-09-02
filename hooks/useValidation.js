49a2e2ae51d004e5c17e50888260fe4c060a7432
Ernesto Briceño <ejbcode@gmail.com>
2020-09-01 20:43:39 -0400

    saving work, add some styles in the header, create a hooks

---
+++ hooks/useValidation.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const useValidation = (initialState, validate, fn) => {
+  const [value, setValue] = useState(initialState);
+  const [error, setError] = useState({});
+  const [submitForm, setSubmitForm] = useState(false);
+
+  useEffect(() => {
+    if (submitForm) {
+      const noErrors = Object.keys(error).length === 0;
+
+      if (noErrors) {
+        fn();
+      }
+      setSubmitForm(false);
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    setValue({
+      ...state,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const validationErrors = validate(value);
+    setError(validationErrors);
+    setSubmitForm(true);
+  };
+
+  return {
+    value,
+    error,
+    submitForm,
+    handleSubmit,
+    handleChange,
+  };
+};
+
+export default useValidation;
--- pages/createaccount.js
+++ pages/createaccount.js
@@ -1,25 +1,34 @@
 import Layout from "../components/Layout";
+import { Form, Field, InputSubmit } from "../components/ui/Form";
+import { css } from "@emotion/core";
 
 const CreateAccount = () => {
   return (
     <>
       <Layout>
-        <h1>CreateAccount</h1>
-        <form>
-          <div>
+        <h1
+          css={css`
+            text-align: center;
+            margin-top: 5rem;
+          `}
+        >
+          CreateAccount
+        </h1>
+        <Form>
+          <Field>
             <label htmlFor="name">Name</label>
             <input type="text" id="name" placeholder="Name" />
-          </div>
-          <div>
+          </Field>
+          <Field>
             <label htmlFor="email">email</label>
             <input type="email" id="email" placeholder="Name" />
-          </div>
-          <div>
+          </Field>
+          <Field>
             <label htmlFor="password">Password</label>
             <input type="password" id="password" />
-          </div>
-          <input type="submit" value="Create an Account" />
-        </form>
+          </Field>
+          <InputSubmit type="submit" value="Create an Account" />
+        </Form>
       </Layout>
     </>
   );
